d128dc17c738e23ac5a35ee4a820319b17dfb595
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-02-27 20:25:48 -0500

    commit tree dc4a96751a8523616bdc5deb27c151836ea54754

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
   
+    def actors_list
+      array = ["#{self.characters.first.actor.first_name} #{self.characters.first.actor.last_name}"]
+      array
+    end
 end
---
+++ db/migrate/006_create_catchphrase_to_characters.rb
@@ -0,0 +1,5 @@
+class CreateCatchphraseToCharacters < ActiveRecord::Migration[5.2]
+    def change
+        add_column :characters, :catchphrase, :string
+    end
+end
